07e21bb168f7bf40c49de517ad6cb589eb2a8442
Mikeleane <servilingual@gmail.com>
2025-08-04 13:35:32 +0200

    Set up

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Your main JS file
+  output: {
+    filename: 'bundle.js', // This is the compiled file name
+    path: path.resolve(__dirname, 'dist') // This is where the file goes
+  },
+  mode: 'development', // Change to 'production' for optimized builds
+};
